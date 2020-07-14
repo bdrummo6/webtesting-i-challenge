296781e8afb8d072d5e0ba0daa161ace0c98cefa
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-13 21:47:35 -0400

    Tested repair function
    Created a test for the repair function and then completed the repair function in enhancer.js. Test passed.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+// Object used for initial testing for each function
+const item = {
+	name: 'football',
+	enhancement: 12,
+	durability: 62,
+};
+
+describe('enhancer unit tests', () => {
+	describe('repair function test', () => {
+		it('test repair function', () => {
+			// Calling the repair function should result in the item's durability to be 100
+			expect(enhancer.repair(item).durability).toBe(100); // 62 to 100
+		});
+	});
+});
