4324b32bf0eaa0afe0a350df4604b52edda6864c
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-14 10:33:40 -0400

    Implemented all enhancer functions for MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,25 +6,34 @@ module.exports = {
 };
 
 function succeed(item) {
-  if(item.enhancement === 20) {
-    item.enhancement = 20;
+  if (item.enhancement === 20) {
+    return { ...item };
   } else {
-    item.enhancement += 1;
+    return { ...item, enhancement: item.enhancement + 1 };
   }
-
-  return { ...item };
-
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement >= 15) {
+    if (item.enhancement > 16) {
+      return {
+        ...item,
+        enhancement: item.enhancement - 1,
+        durability: item.durability - 10,
+      };
+    }
+    return { ...item, durability: item.durability - 10 };
+  }
   return { ...item };
 }
 
 function repair(item) {
-  item.durability = 100;
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
   return { ...item };
 }
+
