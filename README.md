3e53ed421537dfd3589c278baa4fe50295bfa6d0
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-14 19:51:56 -0400

    Added expect statments for all each test item's properties
    All tests passed. MVP complete.

--- README.md
+++ README.md
@@ -46,29 +46,31 @@ After a set of interviews with the potential users of the solution, we gathered 
 
 - [X] a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. This method is the
  simplest of the three and would be a **good starting point** on this project.
-- [ ] a `succeed(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement success.
-- [ ] a `fail(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement failure.
+- [X] a `succeed(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the
+ client for enhancement success.
+- [X] a `fail(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the
+ client for enhancement failure.
 - [ ] a `get()` method for use when working on the stretch problem.
 
 The following sections list information provided by the client about `items` and the game's enhancing system.
 
 ### Items.
 
-- Items have `name`, `durability` and `enhancement`.
-- The item's `enhancement` it's a number from 0 to 20.
-- The item's `durability` it's a number from 0 to 100.
+- [X] Items have `name`, `durability` and `enhancement`.
+- [X] The item's `enhancement` it's a number from 0 to 20.
+- [X] The item's `durability` it's a number from 0 to 100.
 
 ### When enhancement succeeds
 
-- The item's enhancement increases by 1.
-- If the item enhancement level is 20, the enhancement level is not changed.
-- The durability of the item is not changed.
+- [X] The item's enhancement increases by 1.
+- [X] If the item enhancement level is 20, the enhancement level is not changed.
+- [X] The durability of the item is not changed.
 
 ### When enhancement fails
 
-- If the item's enhancement is less than 15, the durability of the item is decreased by 5.
-- If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
-- If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+- [X] If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+- [X] If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+- [X] If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
 
 ## Stretch Problem
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -10,51 +10,62 @@ const item = {
 
 describe('enhancer unit tests', () => {
 	describe('repair() function test', () => {
+		// Calling the repair() function should result in the item's durability to be 100 and enhancement should remain 12
 		it('test repair() function', () => {
-			// Calling the repair() function should result in the item's durability to be 100
+			expect(enhancer.repair(item).name).toBe('football'); // 'football' to 'football'
+			expect(enhancer.repair(item).enhancement).toBe(12); // 12 to 12
 			expect(enhancer.repair(item).durability).toBe(100); // 62 to 100
 		});
 	});
 
 	describe('succeed() function tests',  () => {
+		// Enhancement is 12 after running succeed() the enhancement should be 13 and durability should remain 62
 		it('test succeed() function when enhancement is less than 20', () => {
-			// Enhancement is 12 after running succeed() the enhancement should be 13
+			expect(enhancer.succeed(item).name).toBe('football'); // 'football' to 'football'
 			expect(enhancer.succeed(item).enhancement).toBe(13); // 12 to 13
+			expect(enhancer.succeed(item).durability).toBe(62); // 62 to 62
 		});
 
+		// Enhancement is 20 and after running succeed() the enhancement should remain 20 and durability remain 62
 		it('test succeed() function when enhancement is equal to 20', () => {
 			const newItem = {
 				...item,
 				enhancement: 20,
 			};
-			// Enhancement is now 20 after running succeed() the enhancement value should remain 20
+			expect(enhancer.succeed(newItem).name).toBe('football'); // 'football' to 'football
 			expect(enhancer.succeed(newItem).enhancement).toBe(20); // 20 to 20
+			expect(enhancer.succeed(newItem).durability).toBe(62); // 62 to 62
 		});
 	});
 
 	describe('fail() function tests', () => {
+		// Enhancement of item is 12 so durability should decrease by 5 from 62 to 57 and enhancement remains 12
 		it('test fail() function when enhancement less than 15', () => {
-			// Enhancement of item is 12 so durability should decrease by 5 from 62 to 57
+			expect(enhancer.fail(item).name).toBe('football'); // 'football' to 'football
+			expect(enhancer.fail(item).enhancement).toBe(12); // 12 to 12
 			expect(enhancer.fail(item).durability).toBe(57); // 62 to 57
 		});
 
+		// Enhancement is 15 so durability should decrease by 10 and enhancement and enhancement should remain 15
 		it('test fail() function when enhancement is equal to 15', () => {
 			const newItem = {
 				...item,
 				enhancement: 15,
 				durability: 32,
 			};
-			// Enhancement is 15 so durability should decrease by 10
+			expect(enhancer.fail(newItem).name).toBe('football'); // 'football' to 'football
+			expect(enhancer.fail(newItem).enhancement).toBe(15); // 15 to 15
 			expect(enhancer.fail(newItem).durability).toBe(22); // 32 to 22
 		});
 
+		// Enhancement is 18 so durability decreases by 10 and enhancement decreases by 1
 		it('test fail() function when enhancement is greater than 16', () => {
 			const newItem = {
 				...item,
 				enhancement: 18,
 				durability: 75,
 			};
-			// Enhancement of newItem is 18 so durability decreases by 10 and enhancement decreases by 1
+			expect(enhancer.fail(newItem).name).toBe('football'); // 'football' to 'football
 			expect(enhancer.fail(newItem).enhancement).toBe(17); // 18 to 17
 			expect(enhancer.fail(newItem).durability).toBe(65); // 75 to 65
 		});
