31d5af4e58af67968fd15d676f8ceb46777dba91
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-13 21:56:50 -0400

    Created tests for succeed function
    Created tests when an item's enhancement is less than 20 and when it is equal to 20.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -15,4 +15,20 @@ describe('enhancer unit tests', () => {
 			expect(enhancer.repair(item).durability).toBe(100); // 62 to 100
 		});
 	});
+
+	describe('succeed function tests',  () => {
+		it('test succeed function when enhancement is less than 20', () => {
+			// Enhancement is 12 after running succeed the enhancement should be 13
+			expect(enhancer.succeed(item).enhancement).toBe(13); // 12 to 13
+		});
+
+		it('test succeed function when enhancement is equal to 20', () => {
+			const newItem = {
+				...item,
+				enhancement: 20,
+			};
+			// Enhancement is now 20 after running succeed the enhancement value should remain 20
+			expect(enhancer.succeed(newItem).enhancement).toBe(20); // 20 to 20
+		});
+	});
 });
